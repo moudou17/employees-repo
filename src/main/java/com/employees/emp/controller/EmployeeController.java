5fa96c6a6822378915e1aff3e5072639f89c33e2
mourad <bj_mourad@yahoo.fr>
2020-11-13 15:24:58 +0100

    Rest API to create employee

--- src/main/java/com/employees/emp/controller/EmployeeController.java
+++ src/main/java/com/employees/emp/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.employees.emp.controller;
 import com.employees.emp.model.Employee;
 import com.employees.emp.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,6 +19,12 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
        return employeeRepository.findAll();
+    }
+
+    // Rest API to create employee
+    @PostMapping("/employees")
+    public  Employee createEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
     }
 
 }
